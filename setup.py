628d67f6a86b93e254ba1bc93c22464bef1eba9d
Philip Bauer <bauer@starzel.de>
2020-10-27 20:37:53 +0100

    add minimal package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='minimal',
+    version='0.1',
+    description='An simple debugging view for Plone',
+    url='https://github.com/starzel/minimal',
+    author='Philip Bauer',
+    author_email='bauer@starzel.de',
+    license='GPL version 2',
+    packages=['minimal'],
+    zip_safe=False,
+    entry_points={'z3c.autoinclude.plugin': ['target = plone']},
+    )
